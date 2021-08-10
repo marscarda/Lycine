2ba8cc8391a652d7155c1490f03dc1ae88cc279a
marscarda <marscarda@hotmail.com>
2021-08-10 03:55:36 -0300

    Listing Variables.

--- src/lycine/design/VariableCenter.java
+++ src/lycine/design/VariableCenter.java
@@ -1,8 +1,12 @@
 package lycine.design;
 //************************************************************************
 import methionine.AppException;
+import methionine.TabList;
 import methionine.auth.AuthLamda;
+import methionine.billing.AlterUsage;
 import methionine.billing.BillingLambda;
+import methionine.billing.UsageCost;
+import methionine.project.Project;
 import methionine.project.ProjectLambda;
 import newfactor.survey.variable.Variable;
 import newfactor.survey.variable.VariableLambda;
@@ -19,14 +23,60 @@ public class VariableCenter {
     public void setBillingLambda (BillingLambda billinglambda) { this.billinglambda = billinglambda; }
     public void setVariableLambda (VariableLambda variablelambda) { this.variablelambda = variablelambda; }
     //********************************************************************
-    public void createVariable (Variable variable) throws AppException, Exception {
-        
-        
-        
+    public void createVariable (Variable variable, long userid) throws AppException, Exception {
+        //----------------------------------------------------------------
+        if (variable.getName().length() == 0)
+            throw new AppException("Variable Name cannot be empty", AppException.INVALIDDATASUBMITED);
+        //----------------------------------------------------------------
+        if (variable.getLabel().length() == 0)
+            throw new AppException("Label cannot be empty", AppException.INVALIDDATASUBMITED);
+        //******************************************************************
+        //Reading Part
+        //******************************************************************
+        //We check the user has write acces to the project
+        projectlambda.checkAccess(variable.projectID(), userid, 2);
+        //------------------------------------------------------------------
+        //We recover the project. Needed ahead when altering usage.
+        Project project = projectlambda.getProject(variable.projectID(), 0);
+        //------------------------------------------------------------------
+        //We persist the cost of this particular variable.
+        variable.setCost(UsageCost.VARIABLE);
+        //******************************************************************
+        //Writing Part
+        //******************************************************************
+        //Lock All Tables
+        TabList tabs = new TabList();
+        variablelambda.addCreateVariableLock(tabs);
+        billinglambda.AddLockAlterUsage(tabs);
+        variablelambda.setAutoCommit(0);
+        variablelambda.lockTables(tabs);
+        //------------------------------------------------------------------
         variablelambda.createVariable(variable);
+        //------------------------------------------------------------------
+        //We alter the usage cost.
+        AlterUsage alter = new AlterUsage();
+        alter.setProjectId(project.projectID());
+        alter.setProjectName(project.getName());
+        alter.setIncrease(UsageCost.VARIABLE);
+        alter.setStartingEvent("Variable '" + variable.getName() + "' Created");
+        billinglambda.alterUsage(alter);
+        //------------------------------------------------------------------
+        //We are done.
+        variablelambda.commit();
+        variablelambda.unLockTables();
+        //------------------------------------------------------------------
+    }
+    //********************************************************************
+    public Variable[] getVariables (long projectid, int type, long userid) throws AppException, Exception {
+        //******************************************************************
+        //We check the user has read acces to the project
+        projectlambda.checkAccess(projectid, userid, 1);
+        //------------------------------------------------------------------
         
         
-
+        //******************************************************************
+        return variablelambda.getVariables(projectid, type);
+        //******************************************************************
     }
     //********************************************************************
 }
