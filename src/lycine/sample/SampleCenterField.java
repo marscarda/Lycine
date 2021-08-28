251ccc4fa58207a9d2474b1717706155bb644e73
marscarda <marscarda@hotmail.com>
2021-08-28 02:37:22 -0300

    Methods for get questions added

--- src/lycine/sample/SampleCenterField.java
+++ src/lycine/sample/SampleCenterField.java
@@ -2,6 +2,8 @@ package lycine.sample;
 //************************************************************************
 import methionine.AppException;
 import methionine.auth.AuthErrorCodes;
+import tryptophan.design.Form;
+import tryptophan.design.FormQuestion;
 import tryptophan.sample.Sample;
 //************************************************************************
 public class SampleCenterField extends SampleCenterPanel {
@@ -33,6 +35,39 @@ public class SampleCenterField extends SampleCenterPanel {
         if (sample.userID() != userid)
             throw new AppException("Unauthorized", AuthErrorCodes.UNAUTHORIZED);
         return sample;
+    }
+    //********************************************************************
+    /**
+     * 
+     * @param sampleid
+     * @param userid
+     * @return
+     * @throws AppException
+     * @throws Exception 
+     */
+    public Form getFormBySample (long sampleid, long userid) throws AppException, Exception {
+        Sample sample = samplelambda.getSample(sampleid);
+        if (sample.userID() != userid)
+            throw new AppException("Unauthorized", AuthErrorCodes.UNAUTHORIZED);
+        Form form = designlambda.getQuestionnaire(sample.formID());
+        return form;
+    }
+    //********************************************************************
+    /**
+     * 
+     * @param sampleid
+     * @param userid
+     * @return
+     * @throws AppException
+     * @throws Exception 
+     */
+    public FormQuestion[] getQuestionsBySample (long sampleid, long userid) throws AppException, Exception {
+        Sample sample = samplelambda.getSample(sampleid);
+        if (sample.userID() != userid)
+            throw new AppException("Unauthorized", AuthErrorCodes.UNAUTHORIZED);
+        Form form = designlambda.getQuestionnaire(sample.formID());
+        FormQuestion[] questions = designlambda.getFormQuestions(form.questionaryID());
+        return questions;
     }
     //********************************************************************
 }
