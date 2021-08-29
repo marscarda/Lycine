73b5adadfdcd06a9e512dee361832c187f6fb76a
marscarda <marscarda@hotmail.com>
2021-08-29 15:30:27 -0300

    Public View Response Constants.

---
+++ src/lycine/sample/ConstVarValues.java
@@ -0,0 +1,12 @@
+package lycine.sample;
+//**********************************************************************
+public class ConstVarValues {
+    //====================================================
+    //Public View values.
+    public static int PUBVIEW_POSITIVE = 1;
+    public static int PUBVIEW_NEUTRAL = 2;
+    public static int PUBVIEW_NEGATIVE = 3;
+    public static int PUBVIEW_UNKNOWN = 4;
+    //====================================================
+}
+//**********************************************************************
