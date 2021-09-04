01e71c333d4d4d5258dd0b83815973a44073ed34
marscarda <52551131+marscarda@users.noreply.github.com>
2021-09-04 05:50:38 -0300

    Public View Calculation On JSON in page OK.

--- src/lycine/viewmake/VarStatAlpha.java
+++ src/lycine/viewmake/VarStatAlpha.java
@@ -8,8 +8,9 @@ public class VarStatAlpha {
     int variabletype = 0;
     String label = null;
     //*******************************************
-    
-    //*******************************************
+    public long variableID () { return variableid; }
+    public int variableType () { return variabletype; }
+    //===========================================
     public String getLabel () {
         if (label == null) return "";
         return label;
--- src/lycine/viewmake/VarStatPublicView.java
+++ src/lycine/viewmake/VarStatPublicView.java
@@ -1,9 +1,6 @@
 package lycine.viewmake;
 //************************************************************************
-
 import lycine.sample.ConstVarValues;
-
-
 //************************************************************************
 public class VarStatPublicView extends VarStatAlpha {
     //***********************************
@@ -11,6 +8,12 @@ public class VarStatPublicView extends VarStatAlpha {
     int neutrals = 0;
     int negativees = 0;
     int unknowns = 0;
+    //-----------------------------------
+    int proppositive = 0;
+    int propneutral = 0;
+    int propnegative = 0;
+    int propunknown = 0;
+    int propknowledge = 0;
     //***********************************
     void setValue (int value) {
         switch (value) {
@@ -20,6 +23,37 @@ public class VarStatPublicView extends VarStatAlpha {
             case ConstVarValues.PUBVIEW_UNKNOWN: unknowns++; break;
         }
     }
+    //***********************************
+    public int getPositives () { return positives; }
+    public int getNeutrals () { return neutrals; }
+    public int getNegatives () { return negativees; }
+    public int getUnknowns () { return unknowns; }
+    //***********************************
+    public void doTotalCalculate () {
+        int total = positives + neutrals + negativees + unknowns;
+        float prop = (float)positives / (float)total;
+        proppositive = (int)(prop * (float)100);
+        prop = (float)neutrals / (float)total;
+        propneutral = (int)(prop * (float)100); 
+        prop = (float)negativees / (float)total;
+        propnegative = (int)(prop * (float)100);
+        propunknown = 100 - proppositive - propneutral - propnegative;
+        propknowledge = 100 - propunknown;
+    }
+    public void doOnKnowledgeCalculate () {
+        int total = positives + neutrals + negativees;
+        float prop = (float)positives / (float)total;
+        proppositive = (int)(prop * (float)100);
+        prop = (float)negativees / (float)total;
+        propnegative = (int)(prop * (float)100); 
+        propneutral = 100 - proppositive - propnegative;
+    }
+    //***********************************
+    public int propPositive () { return proppositive; }
+    public int propNeutral () { return propneutral; }
+    public int propNegative () { return propnegative; }
+    public int propUnknown () { return propunknown; }
+    public int getKnowledge () { return propknowledge; }
     //***********************************
 }
 //************************************************************************
