657b31f88f0e075942322a526d63d9005c757801
marscarda <52551131+marscarda@users.noreply.github.com>
2021-10-11 19:37:59 -0300

    Map Record draw in Node advance

--- src/lycine/misc/MapReadDirecter.java
+++ src/lycine/misc/MapReadDirecter.java
@@ -2,13 +2,14 @@ package lycine.misc;
 //************************************************************************
 import threonine.midlayer.MapRecordGraphic;
 import histidine.AurigaObject;
+import java.util.ArrayList;
+import java.util.List;
 import methionine.AppException;
 import threonine.map.MapFolder;
-import threonine.map.MapRecord;
 import threonine.midlayer.MapGraphicGetParam;
 import threonine.midlayer.MapReaderGraphic;
-import threonine.midlayer.MapReaderGraphic;
 import threonine.universe.Universe;
+import tryptophan.trial.StatNode;
 //************************************************************************
 /**
  *
@@ -18,12 +19,10 @@ public class MapReadDirecter {
     //********************************************************************
     AurigaObject auriga = null;
     public void setAuriga (AurigaObject auriga) { this.auriga = auriga; }
-    
     //********************************************************************
     public MapRecordGraphic[] getMapRecords (MapGraphicGetParam param, long userid) throws AppException, Exception {
         //==========================================================
         MapReaderGraphic mapreader = new MapReaderGraphic();
-        //mapreader.setProjectLambda(auriga.getProjectLambda());
         mapreader.setUniverserLambda(auriga.getUniverseAtlas());
         mapreader.setMapsLambda(auriga.getMapsLambda());
         //==========================================================
@@ -33,14 +32,12 @@ public class MapReadDirecter {
         if (param.universeid != 0)
             return bySubset(param.universeid, param.subsetid, userid);
         //==========================================================
-//        if (param.folderid != 0) 
-//            return recordsByFolder(param.folderid, userid);
-        
-        
-        
-        
-        
+        if (param.trialid != 0)
+            return byTrial(param.trialid, param.nodecode, userid);
+        //==========================================================
+        //No valid param received. Return an empty array.
         return new MapRecordGraphic[0];
+        //==========================================================
     }
     //********************************************************************
     private MapRecordGraphic[] byFolder (long folderid, long userid) throws AppException, Exception {
@@ -63,11 +60,20 @@ public class MapReadDirecter {
         return reader.recordsBySubset(universeid, subsetid, userid);
     }
     //********************************************************************
-    private MapRecordGraphic[] byTrial () {
+    private MapRecordGraphic[] byTrial (long trialid, int nodecode, long userid) throws AppException, Exception {
         
+        //Trial trial = auriga.getTrialAtlas().getTrial(trialid);
         
-        
-        return null;
+        MapReaderGraphic reader = new MapReaderGraphic();
+        reader.setUniverserLambda(auriga.getUniverseAtlas());
+        StatNode[] nodes = auriga.getTrialAtlas().getStatNodes(trialid, nodecode);
+        List<MapRecordGraphic> maprecords = new ArrayList<>();
+        MapRecordGraphic maprecord;
+        for (StatNode node : nodes) {
+            maprecord = reader.subsetGetRecord(node.subsetID());
+            maprecords.add(maprecord);
+        }
+        return maprecords.toArray(new MapRecordGraphic[0]);
     }
     //********************************************************************
 }
