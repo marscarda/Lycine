2cad200bc36fb5ce0f85ae4539a7e028c7ac00eb
marscarda <52551131+marscarda@users.noreply.github.com>
2021-11-01 21:08:28 -0300

    Map Folder Deleted

--- src/lycine/map/ExcMapFolderDelete.java
+++ src/lycine/map/ExcMapFolderDelete.java
@@ -2,9 +2,11 @@ package lycine.map;
 //************************************************************************
 import histidine.AurigaObject;
 import methionine.AppException;
+import methionine.TabList;
+import methionine.billing.AlterUsage;
 import methionine.project.Project;
-
 import threonine.map.MapFolder;
+import threonine.map.MapObject;
 import threonine.map.MapRecord;
 //************************************************************************
 public class ExcMapFolderDelete {
@@ -13,6 +15,7 @@ public class ExcMapFolderDelete {
     long folderid = 0;
     long userid = 0;
     long projectid = 0;
+    float cost = 0;
     //----------------------------------------------
     public void setAuriga (AurigaObject auriga) { this.auriga = auriga; }
     public void setFolderId (long folderid) { this.folderid = folderid; }
@@ -21,19 +24,39 @@ public class ExcMapFolderDelete {
     public void doDelete () throws AppException, Exception {
         //================================================================
         //We check the user has permission to do this.
-        if (userid != 0) {
-            MapFolder folder = auriga.getMapsLambda().getMapFolder(folderid);
+        MapFolder folder = auriga.getMapsLambda().getMapFolder(folderid);
+        if (userid != 0)
             auriga.getProjectLambda().checkAccess(folder.projectID(), userid, 3);
-        }
+        //================================================================
+        Project project = auriga.getProjectLambda().getProject(folder.projectID(), userid);
         //================================================================
         //We need to use the master for this.
         auriga.getMapsLambda().useMaster();
+        //================================================================
+        TabList tabs = new TabList();
+        auriga.getBillingLambda().AddLockAlterUsage(tabs);
+        auriga.getMapsLambda().addLockDeleteFolder(tabs);
         
         
+        auriga.getMapsLambda().setAutoCommit(0);
+        auriga.getMapsLambda().lockTables(tabs);
+        //================================================================
         doFolder(folderid);
+        //================================================================
 
-
-
+        //We alter the usage cost.
+        AlterUsage alter = new AlterUsage();
+        alter.setProjectId(project.projectID());
+        alter.setProjectName(project.getName());
+        alter.setDecrease(cost);
+        alter.setStartingEvent("Map Folder '" + folder.getName() + "' Destroyed");
+        auriga.getBillingLambda().alterUsage(alter);
+        //================================================================
+        //We are all done
+        auriga.getMapsLambda().commit();
+        auriga.getMapsLambda().unLockTables();
+        //================================================================
+        System.out.println(cost);
         //================================================================
     }
     //********************************************************************
@@ -51,19 +74,20 @@ public class ExcMapFolderDelete {
     //********************************************************************
     private void doRecords (long folderid) throws AppException, Exception {
 
-
         MapRecord[] records = auriga.getMapsLambda().getMapRecords(folderid);
         for (MapRecord record : records) {
-            //Do Objects
+            doObjects(record.getID());
         }
-        
         auriga.getMapsLambda().deleteMapRecord(folderid, 0);
+
+    }
+    //********************************************************************
+    private void doObjects (long recordid) throws Exception {
         
-
-
-
-
-
+        MapObject[] objects = auriga.getMapsLambda().getObjectsByRecord(recordid, false);
+        for (MapObject obj : objects) cost += obj.cost;
+        auriga.getMapsLambda().clearMapObjects(recordid);
+        
     }
     //********************************************************************
 }
