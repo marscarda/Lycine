e5e73238b4b177a91d78ef054a6dedaf376a1f4c
marscarda <52551131+marscarda@users.noreply.github.com>
2022-03-05 04:09:26 -0300

    Map Page Ok

--- src/lycine/map/ExcMapRecord.java
+++ src/lycine/map/ExcMapRecord.java
@@ -2,6 +2,8 @@ package lycine.map;
 //************************************************************************
 import histidine.AurigaObject;
 import histidine.auth.ProjectAuth;
+import java.util.ArrayList;
+import java.util.List;
 import methionine.AppException;
 import methionine.auth.Session;
 import threonine.map.MapErrorCodes;
@@ -42,6 +44,36 @@ public class ExcMapRecord {
         //****************************************************************
         //We create the record.
         atlas.createMapRecord(record);
+        //****************************************************************
+    }
+    //********************************************************************
+    /**
+     * 
+     * @param rawrecordids
+     * @return
+     * @throws AppException
+     * @throws Exception 
+     */
+    public MapRecord[] getMapRecord (String rawrecordids) throws AppException, Exception {
+        //****************************************************************
+        if (rawrecordids == null) return new MapRecord[0];
+        if (rawrecordids.length() == 0) return new MapRecord[0];
+        String[] txtrecordids = rawrecordids.split(",");  
+        //****************************************************************
+        MappingAttlas atlas = auriga.getMapsLambda();
+        //****************************************************************
+        List<MapRecord> records = new ArrayList<>();
+        long recordid;
+        //================================================================
+        for (String txtrecordid : txtrecordids) {
+            try { 
+                recordid = Long.parseLong(txtrecordid);
+                records.add(atlas.getMapRecord(recordid)); 
+            }
+            catch (AppException e) { continue; }
+        }
+        //****************************************************************
+        return records.toArray(new MapRecord[0]);        
         //****************************************************************
     }
     //********************************************************************
