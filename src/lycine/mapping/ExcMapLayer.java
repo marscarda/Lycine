479e23640cee82884aebbfd8c6ebf4ed304f0751
marscarda <52551131+marscarda@users.noreply.github.com>
2022-03-15 13:42:49 -0300

    Set For Publish discarded

--- src/lycine/mapping/ExcMapLayer.java
+++ src/lycine/mapping/ExcMapLayer.java
@@ -145,37 +145,7 @@ public class ExcMapLayer {
         //****************************************************************
     }
     //********************************************************************
-    public void setForPublish (long layerid, Session session) throws AppException, Exception {
-        //****************************************************************
-        MappingAttlas atlas = auriga.getMapsLambda();
-        //****************************************************************
-        //We recover the layer where the record is going.
-        MapLayer layer = atlas.getLayer(layerid);
-        //****************************************************************
-        //We check the performing user has access to the project.
-        //We check the auth to do this.
-        ProjectAuth pauth = new ProjectAuth();
-        pauth.setAuriga(auriga);
-        pauth.checkAccess(layer.projectID(), session);
-        //****************************************************************
-        atlas.setForPub(layerid, 1);
-        //****************************************************************
-    }
-    //********************************************************************
     /* ADMIN USE */
-    //********************************************************************
-    /**
-     * 
-     * @param session
-     * @return
-     * @throws AppException
-     * @throws Exception 
-     */
-    public MapLayer[] getForPublishLayers (Session session) throws AppException, Exception {
-        if (!session.isAdmin())
-            throw new AppException("Unauthorized", AuthErrorCodes.UNAUTHORIZED);
-        return auriga.getMapsLambda().forPublishLayers();
-    }
     //********************************************************************
 }
 //************************************************************************
