a924d1aa7a60dc64f3f53f45c907c05fc66e4a02
marscarda <52551131+marscarda@users.noreply.github.com>
2022-06-03 15:23:43 -0300

    Sample set and remove from subsets OK

--- src/lycine/universe/ExcUniverseExtend.java
+++ src/lycine/universe/ExcUniverseExtend.java
@@ -5,9 +5,12 @@ import methionine.AppException;
 import methionine.auth.Session;
 import methionine.project.ProjectErrorCodes;
 import threonine.slot.SampleSlot;
+import threonine.slot.SlotErrorCodes;
 import threonine.universe.SubSet;
 import threonine.universe.Universe;
+import tryptophan.design.DesignErrorCodes;
 import tryptophan.sample.Sample;
+import tryptophan.sample.SampleErrorCodes;
 //**************************************************************************
 public class ExcUniverseExtend extends ExcUniverse {
     //**********************************************************************
@@ -44,6 +47,7 @@ public class ExcUniverseExtend extends ExcUniverse {
         pauth.setAuriga(auriga);
         pauth.checkAccess(universe.projectID(), session, 1);
         //==================================================================
+        SampleSlot slot;
         SubSet[] subsets = auriga.getUniverseAtlas().getSubsets(universe.universeID(), parentid);
         int count = subsets.length;
         //==================================================================
@@ -55,6 +59,18 @@ public class ExcUniverseExtend extends ExcUniverse {
         for (int n = 0; n < count; n++) {
             loads[n] = new SubsetLoad();
             loads[n].subset = subsets[n];
+            try { slot = auriga.getSlotAtlas().getSlot(universe.universeID(), loads[n].subsetID()); }
+            catch (AppException e) {
+                if (e.getErrorCode() == SlotErrorCodes.SAMPLESLOTNOTFOUND) continue;
+                throw e;
+            }
+            //--------------------------------------------------------------
+            try { loads[n].sample = auriga.getSampleLambda().getSample(slot.sampleID()); }
+            catch (AppException e) { 
+                if (e.getErrorCode() == SampleErrorCodes.SAMPLENOTFOUND) continue; 
+                throw e;
+            }
+            //--------------------------------------------------------------
         }
         //==================================================================
         return loads;
@@ -69,6 +85,12 @@ public class ExcUniverseExtend extends ExcUniverse {
         pauth.setAuriga(auriga);
         pauth.checkAccess(universe.projectID(), session, 2);
         //==================================================================
+        
+        
+        
+        
+        
+        //==================================================================
         //We get the sample and check if it is in the same project as the universe.
         Sample sample = auriga.getSampleLambda().getSample(slot.sampleID());
         if (sample.projectID() != universe.projectID())
@@ -79,6 +101,20 @@ public class ExcUniverseExtend extends ExcUniverse {
         auriga.getSlotAtlas().setSample(slot);
         slot.setSample(sample);
         //==================================================================
+    }
+    //**********************************************************************
+    public void removeSampleFromSubset (long universeid, long subsetid, Session session) throws AppException, Exception {
+        Universe universe = auriga.getUniverseAtlas().getUniverse(universeid);
+        //==================================================================
+        //We check the user has write acces to the project where the universe belongs
+        ProjectAuth pauth = new ProjectAuth();
+        pauth.setAuriga(auriga);
+        pauth.checkAccess(universe.projectID(), session, 2);
+        //==================================================================
+        
+        //==================================================================
+        auriga.getSlotAtlas().freeSlot(universeid, subsetid);
+        //==================================================================        
     }
     //**********************************************************************
 }
